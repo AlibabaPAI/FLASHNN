dfd17fe0b59b9f5f9079170fd3c20849ba42eb97
Guo-Peilin <guopeilin.gpl@alibaba-inc.com>
2024-07-11 16:54:37 +0800

    add setup file (#4)

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 python3 setup.py develop
 python3 tests/run_tests.py --test unit
-python3 -m pip uninstall tri_blade -y
+python3 -m pip uninstall flashnn -y
 rm -fr dist
 python3 ./setup.py bdist_wheel
 python3 -m pip install dist/*.whl
