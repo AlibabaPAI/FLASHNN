b41d3ca7b16f6192e7f9c7601d232289bbf21411
Xiafei Qiu <xiafei.qiuxf@alibaba-inc.com>
2024-07-31 15:00:03 +0800

    improve setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,18 @@ setup(
     version="0.1.0",
     description="Flash: Triton Kernel Library for LLM Serving",
     author="Alibaba PAI Team",
+    url="https://github.com/AlibabaPAI/FLASHNN",
     packages=find_packages(),
-    install_requires=[],
-    # include_package_data=True,
-    # package_data={
-    # },
-    # scripts=[
-    # ],
+    setup_requires=[
+        'setuptools'
+    ],
+    install_requires=[
+        "torch",
+        "triton",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
