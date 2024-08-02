e7d4e56db4a5a2a78e8f703d378b6502d6079231
Xiafei Qiu <xiafei.qiuxf@alibaba-inc.com>
2024-08-02 19:01:33 +0800

    improve setup.py (#11)
    * improve setup.py
    
    * add readme

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="flashnn",
-    version="0.1.0",
+    version="0.1.1",
     description="Flash: Triton Kernel Library for LLM Serving",
     author="Alibaba PAI Team",
+    url="https://github.com/AlibabaPAI/FLASHNN",
     packages=find_packages(),
-    install_requires=[],
-    # include_package_data=True,
-    # package_data={
-    # },
-    # scripts=[
-    # ],
+    setup_requires=[
+        'setuptools'
+    ],
+    install_requires=[
+        "torch",
+        "triton",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
