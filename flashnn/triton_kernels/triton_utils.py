cfd7f8bb26d035db2647a1c40f0b5ed23105e94b
Guo-Peilin <guopeilin.gpl@alibaba-inc.com>
2024-08-15 09:16:22 +0000

    update

--- flashnn/triton_kernels/triton_utils.py
+++ flashnn/triton_kernels/triton_utils.py
@@ -1,3 +1,7 @@
+# Copyright 2024 The FLASHNN Authors. All rights reserved.
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
 import json
 import os
 import types
